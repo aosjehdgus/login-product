91e99b660bf71bc62a99259ee0906a265d682eec
kdhsea <kdhsea@gmail.com>
2021-03-09 17:25:09 +0900

    Add inputfield component

--- frontend/src/InputField.js
+++ frontend/src/InputField.js
@@ -6,7 +6,13 @@ class InputField extends React.Component {
 
     return (
       <div className="inputField">
-          abcd
+          <input
+              className = 'input'
+              type = {this.props.type}
+              placeholder ={this.props.placeholder}
+              value ={this.props.value}
+              onChange = { (e) => this.props.onChange(e.target.value)}   
+          />
       </div>
     );
   }
