25571e742718bae56a40bfe48dab3cf6d665d20b
kdhsea <kdhsea@gmail.com>
2021-03-10 13:25:47 +0900

    Revise wrong syntax

--- frontend/src/stores/UserStore.js
+++ frontend/src/stores/UserStore.js
@@ -10,7 +10,7 @@ class UserStore{
 /* login 양식이 로드 되는지 여부 확인하기 위해 loading 추가 */
             loading : true,
 /* boolean(논리) 연산자를 통해서 login 여부 확인 */
-            isLogedIn : false,
+            isLoggedIn : false,
 /* username의 초기 상태는 비어있는 문자열로 지정 */
             username : ''
             
